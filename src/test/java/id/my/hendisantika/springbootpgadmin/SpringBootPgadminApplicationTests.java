c2937b6df73af3e3f4b1016017bf3fed6b7d207a
hendisantika <hendisantika@yahoo.co.id>
2024-10-30 07:48:45 +0700

    feat: Add Test🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/springbootpgadmin/SpringBootPgadminApplicationTests.java
+++ src/test/java/id/my/hendisantika/springbootpgadmin/SpringBootPgadminApplicationTests.java
@@ -1,13 +1,37 @@
 package id.my.hendisantika.springbootpgadmin;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.springbootpgadmin.event.EventRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SpringBootPgadminApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:17-alpine3.20")
+            .withDatabaseName("test")
+            .withUsername("test")
+            .withPassword("test");
+    @Autowired
+    private EventRepository eventRepository;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+
+    @BeforeEach
+    void deleteAll() {
+        eventRepository.deleteAll();
     }
 
 }
