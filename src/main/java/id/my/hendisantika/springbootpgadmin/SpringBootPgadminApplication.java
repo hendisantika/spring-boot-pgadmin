2962dd722bc402311fb9eb42fa291b51fd2d9534
hendisantika <hendisantika@yahoo.co.id>
2024-10-30 07:14:59 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/springbootpgadmin/SpringBootPgadminApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootpgadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPgadminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPgadminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/springbootpgadmin/SpringBootPgadminApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootpgadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPgadminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
